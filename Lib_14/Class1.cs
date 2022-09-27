803f33301d58f9f225058665f25b460026e9be8c
ИСП-31 <-31@LAB17-14>
2022-09-27 14:31:28 +0300

    Добавьте файлы проекта.

---
+++ Lib_14/Class1.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lib_14
+{
+    public class Class1
+    {
+        /// <summary>
+        /// Вычислить сумму целых случайных чисел, распределенных в диапазоне от 55 до 70,
+        /// пока эта сумма не превышает некоторого числа K.
+        /// </summary>
+        /// <param name="k"></param>
+        /// <param name="sum">Сумма</param>
+        /// <param name="numbers">Слагаемые</param>
+        /// <param name="count">Количество чисел</param>
+        public static void RandomSum(int k, out int sum, out string numbers, out int count)
+        {
+            int theRandomNumber;
+            count = 0;
+            sum = 0;
+            numbers = "";
+            Random rand = new Random();
+
+            while (sum < k)
+            {
+                theRandomNumber = rand.Next(55, 70);
+                numbers += theRandomNumber.ToString() + "\n";
+                sum += theRandomNumber;
+                count++;
+            }
+        }
+    }
+}
---
+++ Один/MainWindow.xaml.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using Lib_14;
+
+namespace Один
+{
+    /// <summary>
+    /// Логика взаимодействия для MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        // Нажатие на кнопку "Рассчитать"
+        private void btn_Count_Click(object sender, RoutedEventArgs e)
+        {
+            try
+            {
+                int sum; // Сумма
+                string results; //То, что будет выводиться в TextBox с именем result
+                int count; // Количество чисел
+                Lib_14.Class1.RandomSum(Convert.ToInt32(k.Text), out sum, out results, out count);
+                results += "Сумма: " + sum + "\n" + "Количество сгенерированных чисел: " + count;
+                result.Text = results;
+            }
+            catch(Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+            }
+        }
+
+        // Кнопка "О программе"
+        private void btn_About_Click(object sender, RoutedEventArgs e)
+        {
+            MessageBox.Show("Дунаева М.И.\n\nПрактическая работа №1\n\n Вычислить сумму целых случайных чисел, распределенных в диапазоне от 55 до " +
+                "70, пока эта сумма не превышает некоторого числа K.Вывести на экран" +
+                "сгенерированные числа, значение суммы, и количество сгенерированных чисел.");
+        }
+
+        // Кнопка "выход"
+        private void btn_Exit_Click(object sender, RoutedEventArgs e)
+        {
+            this.Close();
+        }
+    }
+}
